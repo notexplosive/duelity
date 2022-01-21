e6cfb4548e0b0271481a7a58b54c55b4bf47217d
notexplosive <thenotexplosive@gmail.com>
2022-01-20 19:24:43 -0800

    Initial (real) commit

---
+++ Duel/DuelGameCartridge.cs
@@ -0,0 +1,34 @@
+﻿using Duel.Components;
+using Duel.Data;
+using Machina.Engine;
+using Machina.Engine.Assets;
+using Machina.Engine.Cartridges;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duel
+{
+    public class DuelGameCartridge : GameCartridge
+    {
+        public DuelGameCartridge() : base(new Point(1600, 900), ResizeBehavior.KeepAspectRatio)
+        {
+        }
+
+        public override void OnGameLoad(GameSpecification specification, MachinaRuntime runtime)
+        {
+            SceneLayers.BackgroundColor = Color.Black;
+
+            var game = SceneLayers.AddNewScene();
+            var levelActor = game.AddActor("Level");
+
+            new LevelRenderer(levelActor, new Level());
+        }
+
+        public override void PrepareDynamicAssets(AssetLoader loader, MachinaRuntime runtime)
+        {
+
+        }
+    }
+}
--- Duel/Program.cs
+++ Duel/Program.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using Machina.Data;
+using Machina.Engine;
+using MachinaDesktop;
+using Microsoft.Xna.Framework;
+using System;
 
 namespace Duel
 {
     public static class Program
     {
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            using (var game = new Game1())
-                game.Run();
+            MachinaBootstrap.Run(new GameSpecification("Duel-ity", args, new GameSettings(new Point(1600, 900))), new DuelGameCartridge(), ".");
         }
     }
 }
