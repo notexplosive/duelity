b3a21bd4defe48e9dab167f24f65c734eaf43857
notexplosive <thenotexplosive@gmail.com>
2022-01-21 14:51:13 -0800

    Hold to move

--- Duel/Components/KeyboardListener.cs
+++ Duel/Components/KeyboardListener.cs
@@ -15,6 +15,8 @@ namespace Duel.Components
     public class KeyboardListener : BaseComponent
     {
         private readonly BusySignal busySignal;
+        private readonly DirectionalButtons heldDirections = new DirectionalButtons();
+        private float waitTimer;
 
         public Action BufferedAction { get; private set; }
 
@@ -33,24 +35,30 @@ namespace Duel.Components
         {
             if (state == ButtonState.Pressed)
             {
+                this.waitTimer = 0.2f;
+
                 if (key == Keys.Left || key == Keys.A)
                 {
                     DoOrBuffer(LeftPressed);
+                    this.heldDirections.left = true;
                 }
 
                 if (key == Keys.Right || key == Keys.D)
                 {
                     DoOrBuffer(RightPressed);
+                    this.heldDirections.right = true;
                 }
 
                 if (key == Keys.Up || key == Keys.W)
                 {
                     DoOrBuffer(UpPressed);
+                    this.heldDirections.up = true;
                 }
 
                 if (key == Keys.Down || key == Keys.S)
                 {
                     DoOrBuffer(DownPressed);
+                    this.heldDirections.down = true;
                 }
 
                 if (key == Keys.Z || key == Keys.Space)
@@ -58,15 +66,70 @@ namespace Duel.Components
                     DoOrBuffer(ActionPressed);
                 }
             }
+
+            if (state == ButtonState.Released)
+            {
+                if (key == Keys.Left || key == Keys.A)
+                {
+                    this.heldDirections.left = false;
+                }
+
+                if (key == Keys.Right || key == Keys.D)
+                {
+                    this.heldDirections.right = false;
+                }
+
+                if (key == Keys.Up || key == Keys.W)
+                {
+                    this.heldDirections.up = false;
+                }
+
+                if (key == Keys.Down || key == Keys.S)
+                {
+                    this.heldDirections.down = false;
+                }
+            }
         }
 
         public override void Update(float dt)
         {
-            if (BufferedAction != null && this.busySignal.IsFree())
+            if (this.busySignal.IsFree())
             {
-                BufferedAction?.Invoke();
-                BufferedAction = null;
+                if (BufferedAction != null)
+                {
+                    BufferedAction?.Invoke();
+                    BufferedAction = null;
+                }
+                else
+                {
+                    if (this.heldDirections.HasPressed())
+                    {
+                        this.waitTimer -= dt;
+                        if (this.waitTimer < 0)
+                        {
+                            DoOrBuffer(DirectionToAction(this.heldDirections.GetFirstDirection()));
+                            this.waitTimer = 0.05f;
+                        }
+                    }
+                }
             }
+        }
+
+        private Action DirectionToAction(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Up:
+                    return UpPressed;
+                case Direction.Down:
+                    return DownPressed;
+                case Direction.Left:
+                    return LeftPressed;
+                case Direction.Right:
+                    return RightPressed;
+            }
+
+            return null;
         }
 
         public void DoOrBuffer(Action action)
---
+++ Duel/Data/DirectionalButtons.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duel.Data
+{
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left,
+        None
+    }
+
+    public class DirectionalButtons
+    {
+        public bool up;
+        public bool right;
+        public bool down;
+        public bool left;
+
+        public bool HasPressed()
+        {
+            return this.up || this.right || this.down || this.left;
+        }
+
+        public Direction GetFirstDirection()
+        {
+            if (this.up && !this.down) { return Direction.Up; }
+            if (this.down && !this.up) { return Direction.Down; }
+
+            if (this.left && !this.right) { return Direction.Left; }
+            if (this.right && !this.left) { return Direction.Right; }
+
+            return Direction.None;
+        }
+    }
+}
