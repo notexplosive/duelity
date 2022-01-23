ee2828fc465aff5008bfa32d94dad1ef8fae03b0
notexplosive <thenotexplosive@gmail.com>
2022-01-22 16:03:21 -0800

    Move class to own file

--- Duel/Data/Level.cs
+++ Duel/Data/Level.cs
@@ -7,28 +7,6 @@ namespace Duel.Data
 {
     public delegate void EntityEvent(Entity entity);
 
-    public class EntityTemplate
-    {
-        private readonly Tag[] tags;
-
-        public EntityTemplate(params Tag[] tags)
-        {
-            this.tags = tags;
-        }
-
-        public Entity Create(LevelSolidProvider provider)
-        {
-            var entity = new Entity(provider);
-
-            foreach (var tag in tags)
-            {
-                entity.Tags.AddTag(tag);
-            }
-
-            return entity;
-        }
-    }
-
     public class Level
     {
         public event Action TilemapChanged;
