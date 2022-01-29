a17640accc32ba59de8fe582ca7c34b283de7f80
Andrew Murray <murraya2@uw.edu>
2022-01-28 21:05:51 -0800

    replace puppeteering with more generic invoke

---
+++ Duel/Components/TitleScreen.cs
@@ -0,0 +1,12 @@
+using Machina.Components;
+using Machina.Engine;
+
+namespace Duel.Components
+{
+    public class TitleScreen : BaseComponent
+    {
+        public TitleScreen(Actor actor) : base (actor) {
+
+        }
+    }
+}
